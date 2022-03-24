5612da7df3846e8a09a8607238812b70aa6caf5e
badlani <badlanishakir@gmail.com>
2022-03-24 15:27:04 +0530

    News-Application

---
+++ app/src/main/java/com/example/newsapplication/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        image = findViewById(R.id.imageview);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
